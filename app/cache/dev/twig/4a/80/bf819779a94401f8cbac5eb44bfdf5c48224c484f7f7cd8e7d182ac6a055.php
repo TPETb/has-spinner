b97ff22a72f214b081fc390f60fd8b9382682ba5
Alex aka TPETb Tretyakov <alextret@gmail.com>
2015-01-05 14:10:06 +0300

    initial commit

---
+++ app/cache/dev/twig/4a/80/bf819779a94401f8cbac5eb44bfdf5c48224c484f7f7cd8e7d182ac6a055.php
@@ -0,0 +1,50 @@
+<?php
+
+/* TwigBundle:Exception:traces.txt.twig */
+class __TwigTemplate_4a80bf819779a94401f8cbac5eb44bfdf5c48224c484f7f7cd8e7d182ac6a055 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if (twig_length_filter($this->env, $this->getAttribute((isset($context["exception"]) ? $context["exception"] : $this->getContext($context, "exception")), "trace", array()))) {
+            // line 2
+            $context['_parent'] = (array) $context;
+            $context['_seq'] = twig_ensure_traversable($this->getAttribute((isset($context["exception"]) ? $context["exception"] : $this->getContext($context, "exception")), "trace", array()));
+            foreach ($context['_seq'] as $context["_key"] => $context["trace"]) {
+                // line 3
+                $this->env->loadTemplate("TwigBundle:Exception:trace.txt.twig")->display(array("trace" => $context["trace"]));
+                // line 4
+                echo "
+";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['trace'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:traces.txt.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  27 => 4,  25 => 3,  21 => 2,  19 => 1,);
+    }
+}
