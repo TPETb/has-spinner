6f175b43a3950137039bfe1910fc2dea883ec247
Sergey Pavliyckhov <sergey.pavliyckhov@gmail.com>
2015-01-09 13:40:31 +0300

    Added views for quiz results and views for one result and logic

--- src/HAS/SpinnerBundle/Controller/QuizController.php
+++ src/HAS/SpinnerBundle/Controller/QuizController.php
@@ -52,16 +52,19 @@ class QuizController extends Controller
         $categoriesArray = [];
         foreach ($categories as $category) {
             $tmp = [
+                'id' => $category->getId(),
                 'name' => $category->getName(),
                 'sections' => [],
             ];
             foreach ($category->getSections() as $section) {
                 $tmp1 = [
+                    'id' => $section->getId(),
                     'name' => $section->getName(),
                     'questions' => [],
                 ];
                 foreach ($section->getQuestions() as $question) {
                     $tmp2 = [
+                        'id' => $question->getId(),
                         'name' => $question->getName(),
                         'weight' => $question->getWeight(),
                         'value' => $request->get('question_'.$question->getId()),
@@ -82,5 +85,39 @@ class QuizController extends Controller
         $dm->flush();
 
         return $this->redirect($this->generateUrl('quiz_do'));
+    }
+
+    /**
+     * @Route("quiz/results", name="quiz_results")
+     * @Method("GET")
+     */
+    public function resultsAction()
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $results = $dm->getRepository('HASSpinnerBundle:QuizResult')->findAll(array('user.name' => 'test-user'));
+        if (!$results) {
+            throw $this->createNotFoundException('Unable to find quiz results.');
+        }
+        return $this->render('HASSpinnerBundle:Quiz:results.html.twig', array('results' => $results));
+    }
+
+    /**
+     * @Route("quiz/view/{id}", name="quiz_view")
+     * @Method("GET")
+     */
+    public function viewAction($id)
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $result = $dm->getRepository('HASSpinnerBundle:QuizResult')->findOneById($id);
+
+        if (!$result) {
+            throw $this->createNotFoundException('Unable to find quiz result.');
+        }
+
+        $categories = json_decode($result->getResult());
+
+        return $this->render('HASSpinnerBundle:Quiz:view.html.twig', array('categories' => $categories));
     }
 }
